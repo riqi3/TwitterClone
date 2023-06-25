fbb275b3e8e23b82e41cc27a9c2ddd68adeecccd
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-06-25 22:43:29 +0800

    initial commit and setting  up twitter clone constants

---
+++ twitter_clone/lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databseId = '6498507ebdf945dbbd49';
+  static const String projectId = '64984d1f408f3339c5ae';
+  static const String endPoint = 'http://localhost:80/v1';
+}
+
